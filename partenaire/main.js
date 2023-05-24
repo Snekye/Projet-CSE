fe667b514ff62f51d9c504033f94306b89f9dd00
Marcus <101703473+marcusWeb04@users.noreply.github.com>
2023-05-24 22:59:26 +0200

    Add files via upload

--- partenaire/main.js
+++ partenaire/main.js
@@ -7,16 +7,3 @@ function ouvrirModal(){
 function fermerModal(){
    document.querySelector(".modalPartenaire").classList.remove("show");
 }
-
-// observeur
-const observer = new IntersectionObserver((entries) => {
-   entries.forEach((entry)=>{
-      console.log(entry)
-      if  (entry.isIntersecting){
-         entry.target.classList.add('show');
-      }
-   });
-});
-
-const hiddenElements= document.querySelectorAll('.hidden');
-hiddenElements.forEach((el)=> observer.observe(el));
--- partenaire/partenaire.php
+++ partenaire/partenaire.php
@@ -7,18 +7,19 @@ $partenaireNom=[];
 $partenaireLien=[];
 
 // requette pour récupérer les partenaire
-$query->$connexion->prepare("SELECT Id_Partenaire FROM partenaire ");
+$query = $connexion->prepare("SELECT Id_Partenaire FROM partenaire ");
 $query-> execute();
 
 // attribution des valeurs dans le tableau partenaire Image
 $partenaire= $query->fetchAll();
+
 
 foreach($partenaire as $element){
     $partenaire[] =$element['Id_Partenaire'];
 }
 
 // requette de jointure pour les images et les partenaires
-$query->$connexion->prepare("SELECT Nom_Image FROM image,partenaire WHERE image.Id_image=partenaire.Id_Image");
+$query =$connexion->prepare("SELECT Nom_Image FROM image,partenaire WHERE image.Id_image=partenaire.Id_Image");
 $query->execute();
 
 // attribution des valeurs dans le tableau partenaire Image
@@ -29,7 +30,7 @@ foreach ($partenaireImage as $element){
 }
 
 // requette du nom du partenaire
-$query->$connexion->prepare("SELECT Nom_Partenaire FROM Partenaire");
+$query =$connexion->prepare("SELECT Nom_Partenaire FROM Partenaire");
 $query->execute();
 
 // attribution des valeurs dans le tableau partenaire Nom
@@ -40,7 +41,7 @@ foreach($partenaireNom as $element){
 }
 
 // requette des liens du partenaire
-$query->$connexion->prepare("SELECT Lien_Partenaire FROM partenaire");
+$query =$connexion->prepare("SELECT Lien_Partenaire FROM partenaire");
 $query->execute();
 
 // attribution des valeurs dans le tableau lien de partenaire
@@ -50,29 +51,18 @@ foreach($partenaireLien as $element){
     $partenaireLien[]= $element['Lien_Partenaire'];
 }
 
+// requette des liens du partenaire
+$query =$connexion->prepare("SELECT Description_Partenaire FROM partenaire");
+$query->execute();
+
 // attribution des valeurs dans le tableau description de partenaire
 $partenaireDescription= $query->fetchAll();
 
-// Si mon formulaire de description a été soumi, alors je le traite
-if (isset($_POST['envoye'])) {
-    $formulaire = $_POST['envoye'];
-
-	    if (isset($formulaire['id'])) {
-		    try {
-			    $requete = $connexion->prepare('SELECT Description_Partenaire FROM partenaire WHERE partenaire_id = :id');
-			    $requete->bindParam(':id', $formulaire['id']);
-			    $requete->execute();
-
-			    // Redirection vers la page liste.php (on force le raffraichissement de la page)
-			    header('Location: ./back.php');
-		    } catch (\Exception $exception) {
-			    var_dump($exception);
-		}
-	}
+foreach($partenaireDescription as $element){
+    $partenaireDescription[]= $element['Description_Partenaire'];
 }
 
-//On attribut la description dans un variable
-$partenaireDescription=$requete;
+
 
 ?>
 <!DOCTYPE html>
@@ -83,61 +73,56 @@ $partenaireDescription=$requete;
     <link rel="stylesheet" href="partenaire.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>projet cse</title>
+    <script src="main.js"></script>
 </head>
 <body>
     <section id="page-Partenaire">
         <div class="textePartenaire">
             <p>
-                Bienvenue dans notre section consacrée a nos partenaire. C'est ici que vous pouvez découvrir nos nombreux colaborateur qui m'ètent à
-                vos disposition divers offre!
+            Découvrez nos partenaires privilégiés !
+            </br>Nous sommes ravis de vous accueillir dans notre espace dédié aux partenaires. Ici, vous trouverez une liste exclusive de partenaires qui offrent des avantages et des réductions spéciales aux membres du CSE.
             </p>
         </div>
-        <div class="modalPartenaire">
-            <div class="contenaire">
-                <div id="fermerModalPartenaire" onclick="fermerModal()">
-                    <p>&times;</p>
-                </div>
-            </div>
-            <div class="partenaire">
-                <div class="logoPartenaire">
-                    <!-- lien vers le site du partenaire -->
-                    <a href="<?php echo $partenaireLien[$i]['Lien_Partenaire']?>" target="_blank">
-                    <img src="<?php echo $partenaireImage[$i]['Nom_Image'] ?>" alt="nom du partenaire">
-                </div>
-                <div class="titrePartenaire2">
-                    <h2><?php echo $partenaireNom[$i] ['Nom_Partenaire']?></h2>
-                </div>
-            </div>
-            <div class="descriptionPartenaire">
-                <p>
-                    <? echo $partenaireDescription ?>
-                </p>
-            </div>
-        </div>
         <div class="contenaire">
-        <?php
-            for ($i = 0; $i < count($partenaire); $i++){?>
-                <div class="partenaire">
-                    <div class="logoPartenaire">
-                        <!-- lien vers le site du partenaire -->
-                        <a href="<?php echo $partenaireLien[$i]['Lien_Partenaire']?>" target="_blank">
-                            <img src="<?php echo $partenaireImage[$i]['Nom_Image'] ?>" alt="nom du partenaire">
-                        </a>
-                    </div>
-                    <form action="#" method="POST" name="envoye">
-                        <button class="titrePartenaire" onclick="ouvrirModal()">
-                            <!-- ouverture de la modal -->
-                            <input type="hidden" name="envoye[id]" value="<?= $element['Id_Partenaire'] ?>">
-                            <h2><?php echo $partenaireNom[$i] ['Nom_Partenaire']?></h2>
-                        </button>
-                    </form>
-                </div>
             <?php
-            } 
-        ?>
+                for ($i = 0; $i < count($partenaire); $i++){?>
+                    <div class="modalPartenaire">
+                            <div class="fermerModalPartenaire" onclick="fermerModal()">
+                                <p>&times;</p>
+                            </div>
+                        <div class="partenaire">
+                            <div class="logoPartenaire">
+                            <a href="<?php echo $partenaireLien[$i]['Lien_Partenaire']?>" target="_blank">
+                                    <img src="<?php echo $partenaireImage[$i]['Nom_Image'] ?>" alt="nom du partenaire">
+                                </a>
+                            </div>
+                            <div class="titrePartenaire2">
+                                <h2><?php echo $partenaireNom[$i] ['Nom_Partenaire']?></h2>
+                            </div>
+                        </div>
+                        <div class="descriptionPartenaire">
+                            <p>
+                                <?php echo $partenaireDescription[$i] ['Description_Partenaire']?>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="partenaire">
+                        <div class="logoPartenaire">
+                            <!-- lien vers le site du partenaire -->
+                            <a href="<?php echo $partenaireLien[$i]['Lien_Partenaire']?>" target="_blank">
+                                <img src="<?php echo $partenaireImage[$i]['Nom_Image'] ?>" alt="nom du partenaire">
+                            </a>
+                        </div>
+                        <div class="titrePartenaire" onclick="ouvrirModal()">
+                            <!-- ouverture de la modal -->
+                                <h2><?php echo $partenaireNom[$i] ['Nom_Partenaire']?></h2>
+                        </div>
+                    </div>
+        <?php
+            }
+            ?>
         </div>
     </section>
-    <script src="main.js"></script>
 </body>
 <footer>
 </footer>
